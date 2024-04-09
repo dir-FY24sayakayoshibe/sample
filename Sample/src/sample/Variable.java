92c960e860f63f62fb39cdc2988fcfa26d58d75d
dir-FY24sayakayoshibe <sayaka.yoshibe.k491@dirbato.co.jp>
2024-04-09 09:07:16 +0900

    コミット

--- Sample/src/sample/Variable.java
+++ Sample/src/sample/Variable.java
@@ -4,7 +4,7 @@ public class Variable {
 
     public static void main(String[] args) {
        var number1 = 2;
-       System.out.print(number1);
+       System.out.println(number1);
       
        String str1 = "abc";
       
---
+++ Sample/src/sample/practice.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class practice {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+
+      
+        
+        var numbers = new int[] {5,6,7,8,9};
+        for (var number : numbers) {
+            System.out.print(number);
+            
+        }
+    }
+
+}
