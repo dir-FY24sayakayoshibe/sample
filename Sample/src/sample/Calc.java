88a9d24d2ca65eed5336c9df843318bdd145e233
dir-FY24sayakayoshibe <sayaka.yoshibe.k491@dirbato.co.jp>
2024-04-16 09:28:14 +0900

    最初のコミット

--- Sample/src/sample/Calc.java
+++ Sample/src/sample/Calc.java
@@ -3,30 +3,13 @@ package sample;
 public class Calc {
 
     public static void main(String[] args) {
-        var num1 = 23;
-        var num2 = 54;
+        var num1 = 1;
+        var num2 = 2;
+        var num3 = 3;
         
-        //足し算
-        var result1 = num1 + num2;
-        System.out.println("num1 + num2 = " + result1);
-        
-        //引き算
-        var result2 = num1 - num2;
-        System.out.println("num1 - num2 = " + result2);
-        
-        //掛け算
-        var result3 = num1 * num2;
-        System.out.println("num1 * num2 = " + result3);
-        
-        //割り算
-        var result4 = num1 / num2;
-        System.out.println("num1 / num2 = " + result4);
-        
-        //剰余
-        var result5 = num1 % num2;
-        System.out.println("num1 % num2 = " + result5);
-        
- 
+      System.out.println("num1>num2&&num2>num3は" + (num1>num2&&num2>num3));
+      System.out.println("num1+num2||num1+num3は" + (num1==num2||num1==num3));
+      
         
     }
 
---
+++ Sample/src/sample/practicecontinue.java
@@ -0,0 +1,18 @@
+package sample;
+
+public class practicecontinue {
+    public static void main(String[] args) {
+
+  int a = 2;
+
+
+  while (a <= 1024) {
+    System.out.println(a);
+    a = a * 2;
+    
+}
+
+
+}
+    
+   }
